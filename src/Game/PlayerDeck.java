90b088041d99ec2150ac3d3fc26f720231e69a18
Léon Brodbeck <brodbeckleon@gmail.com>
2024-02-07 21:04:23 +0100

    updated toString Method of PlayerDeck

--- src/Game/PlayerDeck.java
+++ src/Game/PlayerDeck.java
@@ -50,14 +50,14 @@ public class PlayerDeck {
     }
     public String toString() {
         String unitsString = "";
-        unitsString += "Base: \t\t\t( " + getBase().getPosition().x + ", " + getBase().getPosition().y + " )" + "\n";
+        unitsString += "Base: \t\t\t(" + getBase().getPosition().x + ", " + getBase().getPosition().y + ")" + "\n";
         unitsString += "\t Health: \t" + getBase().getHealthPoints() + "/" + getBase().getMaxHealthPoints() +"\n";
         unitsString += "Units:\n";
         for (int unitID : units.keySet()) {
             Unit unit = getUnit(unitID);
             Point position = unit.getPosition();
 
-            unitsString += unitID + "\t" + unit.getUnitName() + ": \t( " + position.x + ", " + position.y + " )" + "\n";
+            unitsString += unitID + "\t" + unit.getUnitName() + ": \t(" + position.x + ", " + position.y + ")" + "\n";
             unitsString += "\t Health: \t" + unit.getHealthPoints() + "/" + unit.getMaxHealthPoints() + "\n";
         }
         return unitsString;
