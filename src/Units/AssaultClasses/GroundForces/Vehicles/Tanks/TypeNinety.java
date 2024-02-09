65167bc4c611001bf443cc3df0f7a8a7fc720d15
Léon Brodbeck <brodbeckleon@gmail.com>
2024-02-09 13:32:21 +0100

    removed unused datafields and adjusted values to balance game

--- src/Units/AssaultClasses/GroundForces/Vehicles/Tanks/TypeNinety.java
+++ src/Units/AssaultClasses/GroundForces/Vehicles/Tanks/TypeNinety.java
@@ -5,19 +5,10 @@ import Units.AssaultClasses.GroundForces.Vehicles.AssaultTank;
 import java.awt.*;
 
 public class TypeNinety extends AssaultTank {
-    private final int reloadTime = 1;
-    private final int aimTime = 0;
 
     public TypeNinety(Point position) {
-        super("Type 90" ,position, 5, 600, 1500, 3, 600, 4, 2, 480);
+        super("Type 90" ,position, 5, 600, 1500, 3, 400, 4, 2, 480);
     }
 
-    public int getReloadTime() {
-        return reloadTime;
-    }
-
-    public int getAimTime() {
-        return aimTime;
-    }
 }
 
--- src/Units/AssaultClasses/GroundForces/Vehicles/Tanks/TypeTen.java
+++ src/Units/AssaultClasses/GroundForces/Vehicles/Tanks/TypeTen.java
@@ -5,18 +5,9 @@ import Units.AssaultClasses.GroundForces.Vehicles.AssaultTank;
 import java.awt.*;
 
 public class TypeTen extends AssaultTank {
-    private final int reloadTime = 1;
-    private final int aimTime = 0;
 
     public TypeTen(Point position) {
-        super("Type 10", position, 6, 400, 1200, 3, 500, 5, 3, 480);
+        super("Type 10", position, 6, 400, 1200, 3, 300, 5, 3, 600);
     }
 
-    public int getReloadTime() {
-        return reloadTime;
-    }
-
-    public int getAimTime() {
-        return aimTime;
-    }
 }
