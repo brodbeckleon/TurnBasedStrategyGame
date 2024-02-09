5cd4ac75ff26fcea4582442955fec0ad113b8d38
Léon Brodbeck <brodbeckleon@gmail.com>
2024-02-09 14:22:05 +0100

    added supportclass for later

---
+++ src/Units/SupportClass.java
@@ -0,0 +1,10 @@
+package Units;
+
+import java.awt.*;
+
+public class SupportClass extends Unit {
+
+    public SupportClass(String name, Point position, int resourceCost, int healthPoints) {
+        super(name, position, resourceCost, healthPoints);
+    }
+}
