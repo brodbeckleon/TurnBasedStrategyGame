891ad8e9902a5dcb05ac760fd80660c6bb83a923
Léon Brodbeck <brodbeckleon@gmail.com>
2024-02-09 19:58:15 +0100

    made SupportClass abstract

--- src/Units/SupportClass.java
+++ src/Units/SupportClass.java
@@ -2,7 +2,7 @@ package Units;
 
 import java.awt.*;
 
-public class SupportClass extends Unit {
+public abstract class SupportClass extends Unit {
 
     public SupportClass(String name, Point position, int resourceCost, int healthPoints) {
         super(name, position, resourceCost, healthPoints);
