544a2def531900433f67a1e6851986cfc659e6f4
Léon Brodbeck <brodbeckleon@gmail.com>
2024-02-10 14:42:15 +0100

    refactoring of AddCommand Class

--- src/Game/Commands/AddCommand.java
+++ src/Game/Commands/AddCommand.java
@@ -1,5 +1,6 @@
 package Game.Commands;
 
+import Enums.UnitEnum;
 import Game.*;
 import Units.Unit;
 
@@ -7,35 +8,61 @@ import java.awt.*;
 
 public class AddCommand extends Command {
     private final UnitFactory unitFactory= new UnitFactory();
+
     public AddCommand(ConsoleIO consoleIO, Battlefield battlefield, Player playerOne, Player playerTwo){
         super(consoleIO, battlefield, playerOne, playerTwo);
     }
 
     public void addUnit(Player player) {
-        getConsoleIO().print("What unit do you want to add?");
-        String unitName = getConsoleIO().readUnitName();
+        String unitName = getUnitName();
+        boolean isMatch = false;
+
+        for (UnitEnum unit : UnitEnum.values()) {
+            if (unitName.equals(unit.name())) {
+                isMatch = true;
+                break;
+            }
+        }
+
+        if (isMatch) {
+            Point point = choosePosition(player);
+            Unit unit = unitFactory.createUnit(unitName, point);
+            addUnitToTable(unit, player);
+        } else {
+            getConsoleIO().printError("Unknown unit name. Please try again");
+            addUnit(player);
+        }
+    }
+
+    private String getUnitName() {
+        getConsoleIO().print("Which unit do you want to add?");
+        return getConsoleIO().readUnitName();
+    }
+
+    private Point choosePosition(Player player) {
         Point position = readPoint();
 
-        if (checkIfPositionIsFree(position)){
-            if (checkIfPositionIsDeployable(player ,position)) {
-                Unit unit = unitFactory.createUnit(unitName, position);
+        if (checkIfPositionIsFree(position)) {
+            if (checkIfPositionIsDeployable(player, position)) {
+                return position;
 
-                if (unit != null) {
-
-                    if (player.getResourcePoints() >= unit.getResourceCost()) {
-                        player.getPlayerDeck().addUnit(unit);
-                        player.removeResourcePoints(unit.getResourceCost());
-                    } else {
-                        getConsoleIO().printError("Not enough resource points!");
-                    }
-                } else {
-                    getConsoleIO().printError("Invalid unit type!");
-                }
             } else {
                 getConsoleIO().printError("Position is not in deployment radius of the base!");
+                choosePosition(player);
             }
         } else {
             getConsoleIO().printError("Position is not free!");
+            choosePosition(player);
+        }
+        return null;
+    }
+
+    private void addUnitToTable(Unit unit, Player player) {
+        if (player.getResourcePoints() >= unit.getResourceCost()) {
+            player.getPlayerDeck().addUnit(unit);
+            player.removeResourcePoints(unit.getResourceCost());
+        } else {
+            getConsoleIO().printError("Not enough resource points!");
         }
     }
 }
