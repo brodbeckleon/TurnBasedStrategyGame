5cf8a1d356e347d166ed86172071a2a6a19cac58
Léon Brodbeck <brodbeckleon@gmail.com>
2025-10-31 23:47:08 +0900

    rebalancing

--- app/src/main/java/game/units/assaultClasses/ships/Yamato.java
+++ app/src/main/java/game/units/assaultClasses/ships/Yamato.java
@@ -6,6 +6,6 @@ import java.awt.*;
 
 public class Yamato extends Ship {
     public Yamato(int playerID, Point position) {
-        super(playerID, "Yamato", position, 8, 6500, 5, 1, 1500);
+        super(playerID, "Yamato", position, 8, 5000, 5, 1, 1500);
     }
 }
